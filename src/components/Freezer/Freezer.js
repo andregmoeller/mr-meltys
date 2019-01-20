89bc36cd817183c3e86baa92468704bb06bd8517
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-01-20 21:17:40 +0100

    Links the Freezer UI component to the store

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 
 import Panel from '../Panel/Panel';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
+import store from '../../store';
 
 class Freezer extends Component {
+  state = {
+    flavors: store.getState().freezer.flavors,
+  };
+
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState({
+        flavors: store.getState().freezer.flavors,
+      });
+    });
+  }
+
   render() {
+    const flavors = Object.keys(this.state.flavors).map(flavorName => (
+      <FreezerFlavor flavorName={flavorName} scoops={this.state.flavors[flavorName]} key={flavorName} />
+    ));
+        
     return (
       <Panel title="Freezer (°0C)">
-        <FreezerFlavor flavorName="Vanilla" scoops={50} />
+          {flavors}
       </Panel>
     );
   }

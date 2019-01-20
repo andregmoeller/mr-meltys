1229a6d9a9af70c6c89aa7eca4867696346fcb42
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2019-01-20 20:16:21 +0100

    Removes logger middleware

--- src/store.js
+++ src/store.js
@@ -2,12 +2,11 @@ import { createStore, combineReducers, applyMiddleware }  from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducer as freezer } from './ducks/freezer';
-import logger from './middleware/logger';
 
 const rootReducer = combineReducers({
     freezer,
 });
 
 export default createStore(rootReducer, composeWithDevTools(
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
 ));
